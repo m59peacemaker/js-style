c2ace044f10317c23250dc65e9f6e01ca54f32d9
Johnny Hauser <johnnyhauser@gmail.com>
2015-10-12 21:52:14 -0500

    real tests and some impovements

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var merge          = require('lodash/object/merge');
 var debounce       = require('lodash/function/debounce');
 var resizeDetector = require('element-resize-detector')({
-  callOnAdd: false
+  callOnAdd: false,
+  //strategy: 'scroll'
 });
 var applyStylesFromObject = require('./lib/apply-styles-from-object');
 var styleParser           = require('./lib/style-parser');
@@ -9,15 +10,31 @@ var styleParser           = require('./lib/style-parser');
 module.exports = function(options) {
 
   options = merge({
-    root: document.body,
     attr: 'js-style',
     styleFinder: () => null,
     styleParser: styleParser,
-    debounceWait: 250
+    wait: 250
   }, options);
 
   function applyStyles(element, styles) {
     applyStylesFromObject(element, options.styleParser(styles, element));
+  }
+
+  function processElement(element) {
+    var styles = options.styleFinder(element.getAttribute('js-style'), element);
+    var apply = applyStyles.bind(null, element, styles);
+    apply();
+    if (hasFunction(styles)) {
+      // has function(s) - setup listener
+      var listener = options.wait ? debounce(apply, options.wait) : apply;
+      resizeDetector.listenTo(element, listener);
+    }
+  }
+
+  function hasFunction(styles) {
+    return typeof styles === 'function' ||
+      (Array.isArray(styles) && styles.some(styleItem => typeof styleItem === 'function'))
+    ;
   }
 
   var observer = new MutationObserver(function(mutations) {
@@ -25,18 +42,13 @@ module.exports = function(options) {
       return prev.concat([].slice.call(current.addedNodes));
     }, []).filter((node) => {
       return node.nodeType === Node.ELEMENT_NODE && node.hasAttribute(options.attr);
-    }).forEach((element) => {
-      var styles = options.styleFinder(element.getAttribute('js-style'), element);
-      applyStyles(element, styles);
-      if (styles.some(styleItem => typeof styleItem === 'function')) {
-        // has function(s) - setup listener
-        resizeDetector.listenTo(element, debounce(applyStyles.bind(null, element, styles), options.debounceWait));
-      }
-    });
+    }).forEach(processElement);
   });
 
-  function init() {
-    observer.observe(options.root, {
+  function init(rootElement) {
+    rootElement = rootElement || document.body;
+    [].forEach.call(rootElement.querySelectorAll('['+options.attr+']'), processElement);
+    observer.observe(rootElement, {
       childList: true,
       subtree: true
     });
--- test/index.js
+++ test/index.js
@@ -1,43 +1,86 @@
+var test = require('tape');
+
 var attr = 'js-style';
 
 var styler = require('../')({
   attr: attr,
   styleFinder: (val, element) => {
     return element[attr];
-  }
+  },
+  wait: false
 });
-
-styler.init();
 
 function addStyle(element, style) {
   element.setAttribute(attr, true);
   element[attr] = style;
 }
 
-var elem = document.createElement('div');
-elem.textContent = 'heh hey hey';
-addStyle(elem, [{
-  color: 'red'
-}, function(element) {
-  if (element.parentNode.offsetWidth < 768) {
-    return {
-      color: 'green'
-    };
-  }
-}]);
-document.body.appendChild(elem);
+function setup() {
+  var rootElem = document.createElement('div');
+  document.body.appendChild(rootElem);
+  return {
+    rootElem: rootElem
+  };
+}
 
-var p = document.createElement('p');
-p.textContent = 'YO, G!';
-addStyle(p, [{
-  color: 'red'
-}, function(element) {
-  if (element.parentNode.offsetWidth < 768) {
-    return {
-      color: 'blue'
+function teardown() {
+  document.body.innerHTML = '';
+}
+
+function wait(ms) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, ms)
+  });
+}
+
+test('applies to elements already in the DOM', function(t) {
+  t.plan(1);
+  var state = setup();
+  var elem = document.createElement('div');
+  addStyle(elem, {color: 'red'});
+  state.rootElem.appendChild(elem);
+  styler.init(state.rootElem);
+  t.equal(elem.style.color, 'red');
+  teardown();
+});
+
+test('applies to new elements', function(t) {
+  t.plan(1);
+  var state = setup();
+  styler.init(state.rootElem);
+  var elem = document.createElement('div');
+  addStyle(elem, {color: 'red'});
+  state.rootElem.appendChild(elem);
+  setTimeout(function() {
+    t.equal(elem.style.color, 'red');
+    teardown();
+  });
+});
+
+
+test('responds to window resize', function(t) {
+  t.plan(2);
+
+  var win = window.open("", 'x', "height=500, width=500");
+
+  var elem = document.createElement('div');
+  addStyle(elem, [{color: 'red'}, function(element) {
+    if (element.clientWidth < 200) {
+      return {color: 'blue'};
     }
-  }
-}]);
-setTimeout(() => {
-  elem.appendChild(p);
-}, 1000);
+  }]);
+
+  // give time for window to load
+  wait(100).then(function() {
+    styler.init(win.document.body);
+    win.document.body.appendChild(elem);
+    return wait();
+  }).then(function() {
+    t.equal(elem.style.color, 'red');
+    win.resizeTo(200, 200);
+    return wait(100); // give time for listener to be called
+  }).then(function() {
+    t.equal(elem.style.color, 'blue');
+    win.close();
+  });
+});
