6982ebeb91ff09f9ca1aa30d907cabdef484f20f
Johnny Hauser <johnnyhauser@gmail.com>
2015-10-14 02:49:42 -0500

    yessahh

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
 var merge          = require('lodash/object/merge');
 var debounce       = require('lodash/function/debounce');
+var classList      = require('dom-classlist');
+var jsToCSS        = require('js-to-css');
 var resizeDetector = require('element-resize-detector')({
   callOnAdd: false,
-  //strategy: 'scroll'
+ // strategy: 'scroll'
 });
-var applyStylesFromObject = require('./lib/apply-styles-from-object');
+
 var styleParser           = require('./lib/style-parser');
+
 
 module.exports = function(options) {
 
   options = merge({
     attr: 'js-style',
-    styleFinder: () => null,
+    styleFinder: (value, element) => null,
+    paramsFinder: (value, element) => value,
     styleParser: styleParser,
     wait: 250
   }, options);
 
-  function applyStyles(element, styles) {
-    applyStylesFromObject(element, options.styleParser(styles, element));
+  var stylesheetClass = options.attr+'-stylesheet';
+
+  var i = 0;
+  function applyStyles(element, styles, params) {
+    var result = options.styleParser(styles, params, element) || {};
+    var stylesheet = '';
+    var id = options.attr+'-'+(++i);
+    element.className+= ' '+id;
+    if (result.pseudo) {
+      stylesheet += getPseudosStylesheet(id, result.pseudo);
+      delete result.pseudo;
+    }
+    stylesheet +=  jsToCSS({['.'+id]: result});
+    var styleElem = document.createElement('style');
+    styleElem.className = stylesheetClass;
+    styleElem.textContent = stylesheet;
+    element.appendChild(styleElem);
   }
 
-  function processElement(element) {
-    var styles = options.styleFinder(element.getAttribute('js-style'), element);
-    var apply = applyStyles.bind(null, element, styles);
+  function getPseudosStylesheet(id, pseudos) {
+    return Object.keys(pseudos).reduce(function(prev, pseudo) {
+      var style = {};
+      style['.'+id+pseudo] = pseudos[pseudo];
+      return prev+jsToCSS(style);
+    }, '');
+  }
+
+  function processElement(element, isOld) {
+    var attrValue = element.getAttribute(options.attr);
+    var params    = options.paramsFinder(attrValue, element);
+    var styles    = options.styleFinder(attrValue, element);
+    var apply     = applyStyles.bind(null, element, styles, params);
+    if (isOld) {
+      removeOldStyles(element);
+    }
     apply();
-    if (hasFunction(styles)) {
+    if (!isOld && hasFunction(styles)) {
       // has function(s) - setup listener
       var listener = options.wait ? debounce(apply, options.wait) : apply;
       resizeDetector.listenTo(element, listener);
     }
+  }
+
+  function removeOldStyles(element) {
+    classList(element).remove(new RegExp('^'+options.attr+'-.+'));
+    var elem = element.querySelector('style.'+stylesheetClass);
+    element.removeChild(elem);
   }
 
   function hasFunction(styles) {
@@ -39,18 +77,28 @@ module.exports = function(options) {
 
   var observer = new MutationObserver(function(mutations) {
     mutations.reduce((prev, current) => {
+      if (current.type === 'attributes') {
+        processElement(current.target, true);
+        return prev;
+      }
       return prev.concat([].slice.call(current.addedNodes));
     }, []).filter((node) => {
       return node.nodeType === Node.ELEMENT_NODE && node.hasAttribute(options.attr);
-    }).forEach(processElement);
+    }).forEach(function(element) {
+      processElement(element);
+    });
   });
 
   function init(rootElement) {
     rootElement = rootElement || document.body;
-    [].forEach.call(rootElement.querySelectorAll('['+options.attr+']'), processElement);
+    [].forEach.call(rootElement.querySelectorAll('['+options.attr+']'), function(element) {
+      processElement(element);
+    });
     observer.observe(rootElement, {
       childList: true,
-      subtree: true
+      subtree: true,
+      attributes: true,
+      attributeFilter: [options.attr]
     });
   }
 
--- test/index.js
+++ test/index.js
@@ -1,24 +1,37 @@
 var test = require('tape');
+var merge = require('lodash/object/merge');
+var wait = function(ms) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, ms);
+  });
+};
+
+// ms to wait for styles to be rendered, tweak as needed, tests may fail if too low
+var w = 30;
 
 var attr = 'js-style';
 
-var styler = require('../')({
-  attr: attr,
-  styleFinder: (val, element) => {
-    return element[attr];
-  },
-  wait: false
-});
 
-function addStyle(element, style) {
-  element.setAttribute(attr, true);
+function addStyle(element, style, params) {
+  element.setAttribute(attr, params || true);
   element[attr] = style;
 }
 
-function setup() {
+function setup(stylerOptions) {
+  var styler = require('../')(merge({
+    attr: attr,
+    styleFinder: (value, element) => {
+      return element[attr];
+    },
+    paramsFinder: (value, element) => {
+      return value.split(',');
+    },
+    wait: false
+  }, stylerOptions));
   var rootElem = document.createElement('div');
   document.body.appendChild(rootElem);
   return {
+    styler: styler,
     rootElem: rootElem
   };
 }
@@ -27,11 +40,12 @@ function teardown() {
   document.body.innerHTML = '';
 }
 
-function wait(ms) {
-  return new Promise(function(resolve) {
-    setTimeout(resolve, ms)
-  });
-}
+var rgb = {
+  black: 'rgb(0, 0, 0)',
+  red:   'rgb(255, 0, 0)',
+  blue:  'rgb(0, 0, 255)',
+  transparent: 'rgba(0, 0, 0, 0)'
+};
 
 test('applies to elements already in the DOM', function(t) {
   t.plan(1);
@@ -39,20 +53,20 @@ test('applies to elements already in the DOM', function(t) {
   var elem = document.createElement('div');
   addStyle(elem, {color: 'red'});
   state.rootElem.appendChild(elem);
-  styler.init(state.rootElem);
-  t.equal(elem.style.color, 'red');
+  state.styler.init(state.rootElem);
+  t.equal(getComputedStyle(elem).color, rgb.red);
   teardown();
 });
 
 test('applies to new elements', function(t) {
   t.plan(1);
   var state = setup();
-  styler.init(state.rootElem);
+  state.styler.init(state.rootElem);
   var elem = document.createElement('div');
   addStyle(elem, {color: 'red'});
   state.rootElem.appendChild(elem);
   setTimeout(function() {
-    t.equal(elem.style.color, 'red');
+    t.equal(getComputedStyle(elem).color, rgb.red);
     teardown();
   });
 });
@@ -60,27 +74,180 @@ test('applies to new elements', function(t) {
 
 test('responds to window resize', function(t) {
   t.plan(2);
-
+  var state = setup();
   var win = window.open("", 'x', "height=500, width=500");
 
   var elem = document.createElement('div');
-  addStyle(elem, [{color: 'red'}, function(element) {
+  addStyle(elem, [{color: 'red'}, function(params, element) {
     if (element.clientWidth < 200) {
       return {color: 'blue'};
     }
   }]);
 
   // give time for window to load
-  wait(100).then(function() {
-    styler.init(win.document.body);
+  wait(w*2).then(function() {
+    state.styler.init(win.document.body);
     win.document.body.appendChild(elem);
     return wait();
   }).then(function() {
-    t.equal(elem.style.color, 'red');
+    t.equal(getComputedStyle(elem).color, rgb.red);
     win.resizeTo(200, 200);
-    return wait(100); // give time for listener to be called
+    return wait(w); // give time for listener to be called
   }).then(function() {
-    t.equal(elem.style.color, 'blue');
+    t.equal(getComputedStyle(elem).color, rgb.blue);
     win.close();
+    teardown();
+  });
+});
+
+test('applies dynamic pseudo classes (focus)', function(t) {
+  t.plan(1);
+  var state = setup();
+  var elem = document.createElement('a');
+  elem.href = '';
+  addStyle(elem, [{
+    color: 'red',
+    pseudo: {
+      ':hover': {
+        color: 'blue'
+      },
+      ':focus': {
+        color: 'black'
+      }
+    }
+  }]);
+  state.rootElem.appendChild(elem);
+  state.styler.init(state.rootElem);
+  setTimeout(function() {
+    elem.focus();
+    t.equal(getComputedStyle(elem).color, rgb.black);
+    teardown();
+  }, w);
+});
+
+test('applies state pseudo classes (disabled)', function(t) {
+  t.plan(1);
+  var state = setup();
+  var elem = document.createElement('button');
+  elem.disabled = true;
+  addStyle(elem, [{
+    color: 'red',
+    pseudo: {
+      ':disabled': {
+        color: 'black'
+      }
+    }
+  }]);
+  state.rootElem.appendChild(elem);
+  state.styler.init(state.rootElem);
+  elem.focus();
+  setTimeout(function() {
+    t.equal(getComputedStyle(elem).color, rgb.black);
+    teardown();
+  }, w);
+});
+
+test('applies structual pseudo classes (nth-child)', function(t) {
+  t.plan(2);
+  var state = setup();
+  var first = document.createElement('div');
+  var second = document.createElement('div');
+  var style = [{
+    color: 'red',
+    pseudo: {
+      ':nth-child(2)': {
+        color: 'black'
+      }
+    }
+  }];
+  addStyle(first, style);
+  addStyle(second, style);
+  state.rootElem.appendChild(first);
+  state.rootElem.appendChild(second);
+  state.styler.init(state.rootElem);
+  setTimeout(function() {
+    t.equal(getComputedStyle(first).color, rgb.red);
+    t.equal(getComputedStyle(second).color, rgb.black);
+    teardown();
+  }, w);
+});
+
+test('applies pseudo elements (::after)', function(t) {
+  t.plan(1);
+  var state = setup();
+  var elem = document.createElement('div');
+  addStyle(elem, [{
+    color: 'red',
+    pseudo: {
+      '::after': {
+        content: '"*"'
+      }
+    }
+  }]);
+  state.rootElem.appendChild(elem);
+  state.styler.init(state.rootElem);
+  setTimeout(function() {
+    t.equal(getComputedStyle(elem, '::after').getPropertyValue('content'), '"*"');
+    teardown();
+  }, w);
+});
+
+test('finds params for style functions', function(t) {
+  t.plan(2);
+  var state = setup();
+  var elem = document.createElement('div');
+  addStyle(elem, function(params, element) {
+    t.equal(params[0], 'active');
+    t.equal(params[1], 'light');
+    teardown();
+  }, 'active,light');
+  state.rootElem.appendChild(elem);
+  state.styler.init(state.rootElem);
+});
+
+test('re-styles when attribute value changes', function(t) {
+  t.plan(2);
+  var state = setup();
+  var elem = document.createElement('div');
+  function style(params, element) {
+    if (~params.indexOf('active')) {
+      return {color: 'red'};
+    } else {
+      return {color: 'black'};
+    }
+  }
+  addStyle(elem, style);
+  state.rootElem.appendChild(elem);
+  state.styler.init(state.rootElem);
+  wait(w).then(function() {
+    t.equal(getComputedStyle(elem).color, rgb.black);
+    addStyle(elem, style, 'active,light');
+    return wait(w);
+  }).then(function() {
+    t.equal(getComputedStyle(elem).color, rgb.red);
+    teardown();
+  });
+});
+
+// ensure old styles are ditched each new run
+// add param helper (plugin?)
+// add plugin system?
+// write transpiler
+
+test('old styles are cleaned out on re-style', function(t) {
+  t.plan(3);
+  var state = setup();
+  var elem = document.createElement('div');
+  addStyle(elem, {'background-color': 'red'});
+  state.rootElem.appendChild(elem);
+  state.styler.init(state.rootElem);
+  wait(w).then(function() {
+    t.equal(getComputedStyle(elem).backgroundColor, rgb.red);
+    addStyle(elem, {color: 'blue'});
+    return wait(w);
+  }).then(function() {
+    t.equal(getComputedStyle(elem).backgroundColor, rgb.transparent);
+    t.equal(getComputedStyle(elem).color, rgb.blue);
+    teardown();
   });
 });
